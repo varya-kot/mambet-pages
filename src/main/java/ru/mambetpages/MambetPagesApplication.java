5512b16331f987e07f00dc3bf6b89153479d8d7a
Варвара Гетманская <kotovnav@mail.ru>
2024-04-08 22:49:27 +0300

    init

---
+++ src/main/java/ru/mambetpages/MambetPagesApplication.java
@@ -0,0 +1,13 @@
+package ru.mambetpages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MambetPagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MambetPagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mambetpages/MambetPagesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mambetpages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MambetPagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
