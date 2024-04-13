5a94fc3a136de86b45fb7f8c0d9d7c1edaad0638
fiba <iva.mijava@ya.ru>
2024-04-13 16:53:00 +0300

    Fixes

--- src/main/java/ru/mambetpages/controller/PersonalProfileController.java
+++ src/main/java/ru/mambetpages/controller/PersonalProfileController.java
@@ -3,7 +3,12 @@ package ru.mambetpages.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import ru.mambetpages.dto.GetProfileDto;
 import ru.mambetpages.dto.PutProfileDto;
 
