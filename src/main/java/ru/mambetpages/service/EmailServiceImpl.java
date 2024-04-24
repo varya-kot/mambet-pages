623cbe179f28c578134bb0f562811778f4ab476f
Marina Dvoretskova <smallestproblem@gmail.com>
2024-04-24 14:18:31 +0300

    changed EmailService emailService to private.

--- src/main/java/ru/mambetpages/service/EmailServiceImpl.java
+++ src/main/java/ru/mambetpages/service/EmailServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmailServiceImpl implements EmailService {
-
     @Value("${spring.mail.email_alias}")
     private String emailAlias;
 
